7f44d7d5f09ad9955790e11200044d53ca262494
Tyler A Land <tland9@uw.edu>
2014-08-31 18:47:37 -0700

    Attempts at structure

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+"""
+Parses settings.conf and provides top-level script
+"""
+
+from configparser import SafeConfigParser
+
+def main(argv):
+    conf = parse_settings
+
+def parse_settings(settings)
+    _params = SafeConfigParser(allow_no_value=True)
+    _params.read(settings)
