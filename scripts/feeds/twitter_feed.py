76cf2c5c80a1aa005aaca3ab3ad62bcee480e76f
Tyler A Land <tland9@uw.edu>
2014-09-04 13:57:26 -0700

    Pulls last 20 twitter statuses

--- scripts/feeds/twitter_feed.py
+++ scripts/feeds/twitter_feed.py
@@ -1,15 +1,24 @@
 import twitter
 import json
+import calendar
+from itertools import imap
+
+months = dict((month,num) for num,month in enumerate(calendar.month_abbr))
+
+def clean_row(status):
+    datetime = status.AsDict()['created_at'].split()
+    date = [datetime[-1], months[datetime[1]], datetime[2]]
+    date = '-'.join([str(date[0])] + ['0' + str(d) if len(str(d)) < 2 else str(d) for d in date[1:]])
+    time = datetime[3]
+    tz = datetime[-2]
+    return (date, time, tz, status.AsDict()['text'])
 
 def fetch_twitter(conf):
     api = twitter.Api(consumer_key=conf['consumer_key'],
                       consumer_secret=conf['consumer_secret'],
                       access_token_key=conf['access_token_key'],
                       access_token_secret=conf['access_token_secret'])
-    #print api.VerifyCredentials()
 
     # Returns list of last 20 twitter statuses
     timeline = api.GetUserTimeline()
-    for status in timeline:
-        print(status.AsDict()['text'])
-        print(status.AsDict()['created_at'])
+   return imap(clean_row, timeline):
