22d4cdd4f2194603ddd97f5328df6a5341895491
Tyler A Land <tland9@uw.edu>
2014-09-06 17:38:05 -0700

    Grabs SMS data, does some timezone conversion, still incomplete

--- scripts/feeds/sms_feed.py
+++ scripts/feeds/sms_feed.py
@@ -10,35 +10,47 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 from apiclient import errors
 from oauth2client.file import Storage
-from itertools import imap
+from datetime import datetime
+import itertools
+from datetime import datetime
+from pytz import timezone
 
 months = dict((month,num) for num,month in enumerate(calendar.month_name))
 
-last = ''
 def clean_row(row):
-    global last
     ls = row[0].translate(None,',').split(' ')
-    text = row[1]
-    month = str(months[ls[0]]) # E.g., 'September' -> '9'
-    month = '0' + month if len(month) == 1 else month # '9' -> '09'
-    day = ls[1]
-    year = ls[2]
-    hour = ls[4].split(':')[0] # E.g. '1' from '1:23AM'
-    hour = '0' + hour if len(hour) == 1 else hour
-    minute = ls[4][-4:-2] # E.g. '23' from '1:23AM'
-    second = '00'
-    datetimestring = '-'.join([year, month, day])
-    datetimestring += ' '
-    datetimestring += ':'.join([hour, minute, second])
-    # We don't actually track seconds, but we don't want duplicate-timestamped entries
-    # If this entry is the same as the previous one (except for second) then increment
-    if datetimestring[:-2] == last[:-2]:
-        second = str(int(last[-2:]) + 1)
-        second = '0' + second if len(second) == 1 else second
-        datetimestring = datetimestring[:-2] + second
-    last = datetimestring
-    return("'" + datetimestring + "'," + "'" + text + "'")
-                
+    number = row[1]
+    contact_name = row[2]
+    text = row[3]
+    month = months[ls[0]] # E.g., 'September' -> '9'
+#    month = '0' + month if len(month) == 1 else month # '9' -> '09'
+    day = int(ls[1])
+    year = int(ls[2])
+    hour = int(ls[4].split(':')[0]) # E.g. '1' from '1:23AM'
+#    hour = '0' + hour if len(hour) == 1 else hour
+    minute = int(ls[4][-4:-2]) # E.g. '23' from '1:23AM'
+    second = 0
+#    date = '-'.join([year, month, day])
+#    time = ':'.join([hour, minute, second])
+    dt = datetime(year, month, day, hour, minute, second)
+    dt = timezone('US/Pacific').localize(dt)
+
+    # timezone = 'PST'
+#    fields = ["'" + field + "'" for field in [date, time, timezone, number,
+#                                              contact_name, text]]
+    print(dt)
+    datetimestring = dt.strftime("%Y-%m-%d %H:%M:%S %Z%z")
+    print(datetimestring)
+
+#    dt = datetime.datetime(int(year), int(month), int(day),
+#                           int(hour), int(minute), int(second))
+#    dt = timezone('UTC').localize(dt)
+#    print(dt)
+#    dt = dt.astimezone(timezone('US/Pacific'))
+#    print(dt)
+    return None
+#    return(','.join(fields))
+
 def fetch_sms(conf):
     # https://developers.google.com/drive/web/auth/web-client
 
@@ -52,7 +64,9 @@ def fetch_sms(conf):
     drive_service = build('drive', 'v2', http=http)
     
     results = []
-    params = {'q': 'title="Tweets by @AtumTal"'}
+
+    # Received SMS messages
+    params = {'q': 'title="{}"'.format(conf['sms_received'])}
     files = drive_service.files().list(**params).execute()
     results.extend(files['items'])
 
@@ -63,5 +77,21 @@ def fetch_sms(conf):
     if resp.status != 200:
         print("An error occured: {}".format(resp))
         return None
-    reader = csv.reader(content.split('\n'), delimiter=',', quotechar='"')
-    return(imap(clean_row, reader))
+    recv_reader = csv.reader(content.split('\n'), delimiter=',', quotechar='"')
+
+    # Sent SMS messages
+    params = {'q': 'title="{}"'.format(conf['sms_sent'])}
+    files = drive_service.files().list(**params).execute()
+    results.extend(files['items'])
+
+    assert(2==len(results))
+    # Drive will kindly insert double-quotes if they are already present in a field
+    url = results[0]['exportLinks']['text/csv']
+    resp, content = drive_service._http.request(url)
+    if resp.status != 200:
+        print("An error occured: {}".format(resp))
+        return None
+    sent_reader = csv.reader(content.split('\n'), delimiter=',', quotechar='"')
+
+    return(itertools.chain(itertools.imap(clean_row, recv_reader),
+                           itertools.imap(clean_row, sent_reader)))
