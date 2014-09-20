cc44de9c2e76c30a107e8ee80669780e8c7df031
Tyler A Land <tland9@uw.edu>
2014-09-20 10:20:16 -0700

    Formatting

--- bin/createtables.sql
+++ bin/createtables.sql
@@ -1,4 +1,31 @@
-CREATE table if not exists Twitter(datetime TEXT UNIQUE, content TEXT);
-CREATE table if not exists sms_send(datetime TEXT, timezone TEXT, number TEXT, contact_name TEXT, message TEXT, UNIQUE(datetime, timezone, message) on conflict replace);
-CREATE table if not exists sms_recv(datetime TEXT, timezone TEXT, number TEXT, contact_name TEXT, message TEXT, UNIQUE(datetime, timezone, message) on conflict replace);
-CREATE table if not exists Fitbit_intraday_steps(datetime TEXT UNIQUE, steps INTEGER);
+CREATE table if not exists 
+Twitter(datetime TEXT UNIQUE, 
+        timezone TEXT,
+        message TEXT,
+        UNIQUE(datetime,timezone,message)
+      on conflict replace);
+
+CREATE table if not exists 
+sms_send(datetime TEXT, 
+         timezone TEXT, 
+         phone TEXT, 
+         contact_name TEXT, 
+         message TEXT, 
+         UNIQUE(datetime, timezone, message) 
+        on conflict replace);
+
+CREATE table if not exists 
+sms_recv(datetime TEXT, 
+         timezone TEXT, 
+         phone TEXT, 
+         contact_name TEXT, 
+         message TEXT, 
+         UNIQUE(datetime, timezone, message) 
+        on conflict replace);
+
+CREATE table if not exists 
+Fitbit_intraday_steps(datetime TEXT UNIQUE, 
+                      timezone TEXT, 
+                      steps INTEGER, 
+                      UNIQUE(datetime, timezone) 
+                    on conflict replace);
